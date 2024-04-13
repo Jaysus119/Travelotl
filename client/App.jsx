897d0711382a54f200263f8278fb48470fd6855e
Jay Hoogheem <jayh1186@gmail.com>
2024-04-13 11:35:20 -0500

    added about, login, and header components & routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Main from './components/Main';
 
 export default function App() {
   return (
-    <div>
-      <h1>Travelotl</h1>
-      <p>Let us plan the trip of your dreams...</p>
-    </div>
+    <>
+      <Header />
+      <Main >
+        <Outlet />
+      </Main>
+    </>
+
   );
 }
